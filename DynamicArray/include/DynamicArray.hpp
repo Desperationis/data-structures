22daf222864f2f4ec72c9d52f136de6ef25983ed
Desperationis <smarttdiego@gmail.com>
2022-05-16 09:51:07 -0700

    Fully implement DynamicArray and tests

--- DynamicArray/include/DynamicArray.hpp
+++ DynamicArray/include/DynamicArray.hpp
@@ -2,13 +2,15 @@
 #define DYNARRAY_HPP
 
 #include <memory>
+#include <iostream>
 
 
 template <class T, size_t size>
 class DynamicArray {
 public:
 	DynamicArray() {
-		data = (T*)calloc(size, sizeof(T));
+		currentSize = size;
+		data = (T*)calloc(currentSize, sizeof(T));
 		Clear();
 	}
 
@@ -17,24 +19,42 @@ public:
 	}
 
 	T& operator[](std::size_t index) {
-		if(index < 0 || index >= size)
+		if(index < 0 || index >= currentSize)
 			throw std::out_of_range("Attempted to access element out of bounds!");
 
-		return *(data + index);
+		return *(data + index); // Pointer is start of array
 	}
 
 	const size_t MaxSize() const {
-		return size;
+		return currentSize;
+	}
+
+	/**
+	 * Resizes the array to fit n elements. If there are elements after index
+	 * n-1 after the change, they will be erased. If this results in an
+	 * expansion of space those elements will be set to 0.
+	*/ 
+	void Resize(size_t n) {
+		data = (T*)realloc(data, sizeof(T) * n);
+		if(n > currentSize) {
+			for(size_t i = currentSize; i < n; i++) {
+				// Don't use (*this)[i] as currentSize isn't updated, resulting
+				// in an exception being thrown.
+				*(data + i) = 0; 
+			}
+		}
+		currentSize = n;
 	}
 
 	void Clear() {
-		for(size_t i = 0; i < size; i++) {
-			*(data + i) = 0; // Pointer is start of array
+		for(size_t i = 0; i < currentSize; i++) {
+			(*this)[i] = 0;
 		}
 	}
 
 private:
 	T* data;
+	size_t currentSize;
 };
 
 #endif
--- DynamicArray/src/main.cpp
+++ DynamicArray/src/main.cpp
@@ -41,6 +41,56 @@ TEST(DynamicArrayTest, Populate) {
 	}
 }
 
+TEST(DynamicArrayTest, ResizeSize) {
+	DynamicArray<int, 10> arr;
+	arr.Resize(8);
+	EXPECT_EQ(arr.MaxSize(), 8);
+
+	arr.Resize(400);
+	EXPECT_EQ(arr.MaxSize(), 400);
+}
+
+TEST(DynamicArrayTest, ResizePopulateTest) {
+	DynamicArray<int, 8> arr;
+	for(int i = 0; i < 8; i++) {
+		arr[i] = i;
+	}
+
+	arr.Resize(20);
+	for(int i = 0; i < 8; i++) {
+		EXPECT_EQ(arr[i], i);
+	}
+	for(int i = 8; i < 20; i++) {
+		EXPECT_EQ(arr[i], 0);
+	}
+
+	arr.Resize(5);
+	for(int i = 0; i < 5; i++) {
+		EXPECT_EQ(arr[i], i);
+	}
+}
+
+TEST(DynamicArrayTest, ResizeBounds) {
+	DynamicArray<int, 150> arr;
+	arr.Resize(5);
+	EXPECT_THROW(arr[5], std::out_of_range);
+	EXPECT_THROW(arr[100], std::out_of_range);
+	EXPECT_THROW(arr[-1], std::out_of_range);
+	EXPECT_THROW(arr[-100], std::out_of_range);
+	EXPECT_NO_THROW(arr[0]);
+	EXPECT_NO_THROW(arr[3]);
+	EXPECT_NO_THROW(arr[4]);
+
+	arr.Resize(500);
+	EXPECT_THROW(arr[500], std::out_of_range);
+	EXPECT_THROW(arr[10000], std::out_of_range);
+	EXPECT_THROW(arr[-1], std::out_of_range);
+	EXPECT_THROW(arr[-100], std::out_of_range);
+	EXPECT_NO_THROW(arr[0]);
+	EXPECT_NO_THROW(arr[250]);
+	EXPECT_NO_THROW(arr[499]);
+}
+
 int main(int argc, char** argv) {
 	testing::InitGoogleTest();
 	return RUN_ALL_TESTS();
