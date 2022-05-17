8a87a753d4a7ae04a252f12d4ed2bbb78b69a0fa
Desperationis <smarttdiego@gmail.com>
2022-05-17 08:42:28 -0700

    Add basic print functionality to String

--- String/include/String.hpp
+++ String/include/String.hpp
@@ -3,16 +3,48 @@
 
 #include <memory>
 #include <iostream>
+#include <cstring>
 
 
 class String {
 public:
-	String() {
+	String(const char* str) {
+		if (str == nullptr) {
+			data = new char[1];
+			data[0] = '\0';
+		}
+
+		else {
+			// +1 for \0
+			data = new char[strlen(str) + 1];
+
+			std::strcpy(data, str);
+			data[strlen(str)] = '\0';
+		}
+	}
+
+	String(const String& copy) {
+		data = new char[strlen(copy.data) + 1];
+		std::strcpy(data, copy.data);
+		data[strlen(copy.data)] = '\0';
 	}
 
 	~String() {
+		delete[] data;
 	}
 
+
+
+private:
+	char* data;
+	friend std::ostream& operator<<(std::ostream& os, const String& str);
 };
+
+
+std::ostream& operator<<(std::ostream& os, const String& str)
+{
+    os << str.data;
+    return os;
+}
 
 #endif
--- String/src/main.cpp
+++ String/src/main.cpp
@@ -4,5 +4,10 @@
 
 int main(int argc, char** argv) {
 	testing::InitGoogleTest();
+
+	const char* buf = "Hello world!";
+	String test(buf);
+	std::cout << test << std::endl;
+
 	return RUN_ALL_TESTS();
 }
