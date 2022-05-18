3fa73557850b6e97304f0f7802ab80ee8e5ae980
Desperationis <smarttdiego@gmail.com>
2022-05-18 12:04:04 -0700

    Add basic framework for LinkedList

---
+++ LinkedList/include/LinkedList.hpp
@@ -0,0 +1,15 @@
+#ifndef STRING_HPP
+#define STRING_HPP
+
+#include <memory>
+#include <iostream>
+#include <cstring>
+
+
+class LinkedList {
+public:
+	LinkedList() = default;
+	~LinkedList() = default;
+};
+
+#endif
