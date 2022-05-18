7f82357c4c2e08f37eceea04d6fe8219153b0a9a
Desperationis <smarttdiego@gmail.com>
2022-05-18 11:26:45 -0700

    Fully implement String and unit tests

--- String/include/String.hpp
+++ String/include/String.hpp
@@ -17,7 +17,6 @@ public:
 		else {
 			// +1 for \0
 			data = new char[strlen(str) + 1];
-
 			std::strcpy(data, str);
 			data[strlen(str)] = '\0';
 		}
@@ -33,18 +32,79 @@ public:
 		delete[] data;
 	}
 
+	void Append(const char* c) {
+		std::strcat(data, c);
+	}
 
+	void Pop() {
+		if (strlen(data) == 0)
+			return;
+
+		size_t length = strlen(data);
+
+		// New string is actually one spot smaller since we need to make space
+		// for \0
+		char* buff = new char[length]; 
+
+		strncat(buff, data, length - 1);
+
+		delete[] data;
+		data = buff;
+	}
+
+	void CopyTo(char* str, size_t len, size_t pos) {
+		for(size_t i = 0; i < len; i++) {
+			str[i] = data[pos + i];
+		}
+		str[len] = '\0';
+	}
+
+	size_t Length() {
+		return strlen(data);
+	}
+
+	const char* CStr() {
+		return data;
+	}
+
+	bool operator==(const String& other) const {
+		return strcmp(data, other.data) == 0;
+	}
+	
+	bool operator==(const char* other) const {
+		return strcmp(data, other) == 0;
+	}
+
+	char operator[](size_t index) {
+		if(index < 0 || index >= strlen(data))
+			throw std::out_of_range("Attempted to access character out of bounds!");
+
+		return *(data + index);
+	}
 
 private:
 	char* data;
 	friend std::ostream& operator<<(std::ostream& os, const String& str);
+	friend String operator+(const String& left, const String& right);
+	friend String operator+(const String& left, const char* right);
 };
 
 
-std::ostream& operator<<(std::ostream& os, const String& str)
-{
+std::ostream& operator<<(std::ostream& os, const String& str) {
     os << str.data;
     return os;
+}
+
+String operator+(const String& left, const String& right) {
+	String tmp(left);
+	std::strcat(tmp.data, right.data);
+	return tmp;
+}
+
+String operator+(const String& left, const char* right) {
+	String tmp(left);
+	std::strcat(tmp.data, right);
+	return tmp;
 }
 
 #endif
--- String/src/main.cpp
+++ String/src/main.cpp
@@ -2,12 +2,109 @@
 #include "String.hpp"
 #include <gtest/gtest.h>
 
+TEST(StringTest, CorrectSize) {
+	String test("Hello world!");
+	EXPECT_EQ(test.Length(), 12);
+
+	test.Append("ag");
+	EXPECT_EQ(test.Length(), 14);
+
+	test.Pop();
+	test.Pop();
+	test.Pop();
+	EXPECT_EQ(test.Length(), 11);
+}
+
+TEST(StringTest, Append) {
+	String test("Append");
+	test.Append(" me");
+	EXPECT_EQ(test, "Append me");
+}
+
+TEST(StringTest, AppendPop) {
+	String test("Append");
+	test.Append(" me");
+	EXPECT_EQ(test, "Append me");
+	test.Pop();
+	test.Pop();
+	test.Pop();
+	EXPECT_EQ(test, "Append");
+}
+
+TEST(StringTest, Pop) {
+	String test("Pop");
+	
+	test.Pop();
+	EXPECT_EQ(test, "Po");
+	
+	test.Pop();
+	EXPECT_EQ(test, "P");
+	
+	test.Pop();
+	EXPECT_EQ(test, "");
+
+	test.Pop();
+	EXPECT_EQ(test, "");
+}
+
+TEST(StringTest, Bounds) {
+	String test("Testmybounds"); // len: 12
+	EXPECT_THROW(test[12], std::out_of_range);
+	EXPECT_THROW(test[100], std::out_of_range);
+	EXPECT_THROW(test[-1], std::out_of_range);
+	EXPECT_THROW(test[-100], std::out_of_range);
+	EXPECT_NO_THROW(test[0]);
+	EXPECT_NO_THROW(test[7]);
+	EXPECT_NO_THROW(test[3]);
+}
+
+TEST(StringTest, Access) {
+	String test("accessme");
+	EXPECT_EQ(test[0], 'a');
+	EXPECT_EQ(test[5], 's');
+	EXPECT_EQ(test[7], 'e');
+}
+
+TEST(StringTest, CopyConstructor) {
+	String test("Magic the Gathering");
+	String copy(test);
+	EXPECT_EQ(copy, test);
+	EXPECT_EQ(copy, "Magic the Gathering");
+}
+
+TEST(StringTest, Addition) {
+	String test("Hello");
+	String sum = test + " world!";
+	EXPECT_EQ(sum, "Hello world!");
+
+	String test2("May the force");
+	String test3(" be with you.");
+	String sum2 = test2 + test3;
+	EXPECT_EQ(sum2, "May the force be with you.");
+}
+
+TEST(StringTest, CopyChar) {
+	String test("Magic the Gathering");
+	char* copy = new char[50];
+	test.CopyTo(copy, 5, 0);
+	EXPECT_EQ(std::strcmp(copy, "Magic"), 0);
+	test.CopyTo(copy, 3, 6);
+	EXPECT_EQ(std::strcmp(copy, "the"), 0);
+	delete[] copy;
+}
+
+TEST(StringTest, Empty) {
+	String t(nullptr);
+	//todo
+
+}
+
+
+
+
+
+
 int main(int argc, char** argv) {
 	testing::InitGoogleTest();
-
-	const char* buf = "Hello world!";
-	String test(buf);
-	std::cout << test << std::endl;
-
 	return RUN_ALL_TESTS();
 }
