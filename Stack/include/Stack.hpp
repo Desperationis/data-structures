e8bcf78b9cf0180639baa1d63122a5d2b86bb139
Desperationis <smarttdiego@gmail.com>
2022-05-23 10:19:34 -0700

    Fully Implement Stack and Unit Tests

--- Stack/include/Stack.hpp
+++ Stack/include/Stack.hpp
@@ -4,26 +4,85 @@
 #include <memory>
 #include <iostream>
 
-template <class T>
+template <class T, size_t size = 1>
 class Stack {
 public:
 	Stack() {
 		length = 0;
+		currentSize = size;
+		data = new T[currentSize];
 	}
 
 	~Stack() {
+		delete[] data;
 	};
-/*
-	void Push() {
 
+	void Push(T elm) {
+		if (length >= currentSize) {
+			Realloc();
+		}
 
+		*(data + length) = elm;
+		length++;
+	}
+
+	T Pop() {
+		if (length == 0) {
+			throw std::out_of_range("Attempted to pop element when stack is empty!");
+		}
+
+		// Leave old data intact; It will be overriden anyway
+		length--;
+		return *(data + length);
+	}
+
+	T Peek() {
+		if (length == 0) {
+			throw std::out_of_range("Attempted to peek at element when stack is empty!");
+		}
+
+		return *(data + (length - 1));
+	}
+
+	void Clear() {
+		// Leave old data intact; It will be overriden anyway
+		length = 0;
+	}
+
+	size_t Length() {
+		return length;
+	}
+
+	size_t MaxLength() {
+		return currentSize;
 	}
 
 	T& operator[](size_t index) {
-	}*/
+		if(index < 0 || index >= length)
+			throw std::out_of_range("Attempted to access element out of bounds!");
+
+		return *(data + index);
+	}
+
+private:
+	/**
+	 * Doubles the allocated size for `data` and moves all old data to new
+	 * chunk.
+	*/ 
+	void Realloc() {
+		T* tmp = new T[currentSize * 2];
+		for(size_t i = 0; i < length; i++) {
+			*(tmp + i) = *(data + i);
+		}
+		delete[] data;
+		data = tmp;
+		currentSize *= 2;
+	}
 
 private:
 	size_t length;
+	size_t currentSize;
+	T* data;
 };
 
 #endif
--- Stack/src/main.cpp
+++ Stack/src/main.cpp
@@ -2,8 +2,168 @@
 #include "Stack.hpp"
 #include <gtest/gtest.h>
 
+TEST(StackTest, CorrectRealloc) {
+	Stack<int> s;
+	EXPECT_EQ(s.MaxLength(), 1);
+	for(size_t i = 0; i < 5; i++) {
+		s.Push(i);
+	}
+	EXPECT_EQ(s.MaxLength(), 8);
+
+	Stack<int, 10> s2;
+	EXPECT_EQ(s2.MaxLength(), 10);
+	for(size_t i = 0; i < 11; i++) {
+		s2.Push(i);
+	}
+	EXPECT_EQ(s2.MaxLength(), 20);
+}
+
+TEST(StackTest, CorrectSize) {
+	Stack<int> s;
+	EXPECT_EQ(s.Length(), 0);
+	for(size_t i = 0; i < 14; i++) {
+		s.Push(i);
+	}
+	EXPECT_EQ(s.Length(), 14);
+
+	Stack<int, 10> s2;
+	EXPECT_EQ(s2.Length(), 0);
+	for(size_t i = 0; i < 14; i++) {
+		s2.Push(i);
+	}
+	EXPECT_EQ(s2.Length(), 14);
+}
+
+TEST(StackTest, Populate) {
+	Stack<int> s;
+	for(size_t i = 0; i < 100; i++) {
+		s.Push(i * 2);
+	}
+	for(int i = 99; i >= 0; i--) { // Overflow for size_t
+		EXPECT_EQ(s.Pop(), i * 2);
+	}
+
+	Stack<int, 15> s2;
+	for(size_t i = 0; i < 100; i++) {
+		s2.Push(i * 2);
+	}
+	for(int i = 99; i >= 0; i--) { // Overflow for size_t
+		EXPECT_EQ(s2.Pop(), i * 2);
+	}
+}
+
+TEST(StackTest, Pop) {
+	Stack<int> s;
+	EXPECT_THROW(s.Pop(), std::out_of_range);
+	s.Push(1);
+	EXPECT_NO_THROW(s.Pop());
+	EXPECT_THROW(s.Pop(), std::out_of_range);
+
+	Stack<int, 100> s2;
+	EXPECT_THROW(s2.Pop(), std::out_of_range);
+	s2.Push(1);
+	EXPECT_NO_THROW(s2.Pop());
+	EXPECT_THROW(s2.Pop(), std::out_of_range);
+}
+
+TEST(StackTest, Peek) {
+	Stack<int> s;
+	EXPECT_THROW(s.Peek(), std::out_of_range);
+	s.Push(1);
+	EXPECT_NO_THROW(s.Peek());
+	EXPECT_EQ(s.Peek(), 1);
+	s.Pop();
+	EXPECT_THROW(s.Peek(), std::out_of_range);
+
+	Stack<int, 10> s2;
+	EXPECT_THROW(s2.Peek(), std::out_of_range);
+	s2.Push(1);
+	EXPECT_NO_THROW(s2.Peek());
+	EXPECT_EQ(s2.Peek(), 1);
+	s2.Pop();
+	EXPECT_THROW(s2.Peek(), std::out_of_range);
+}
+
+TEST(StackTest, Access) {
+	Stack<int> s;
+	for(size_t i = 0; i < 100; i++) {
+		s.Push(i);
+	}
+	
+	EXPECT_EQ(s[0], 0);
+	EXPECT_EQ(s[21], 21);
+	EXPECT_EQ(s[68], 68);
+	EXPECT_EQ(s[99], 99);
+
+	EXPECT_THROW(s[-1], std::out_of_range);
+	EXPECT_THROW(s[-100], std::out_of_range);
+	EXPECT_THROW(s[100], std::out_of_range);
+	EXPECT_THROW(s[1000], std::out_of_range);
+
+	EXPECT_NO_THROW(s[0]);
+	EXPECT_NO_THROW(s[10]);
+	EXPECT_NO_THROW(s[52]);
+	EXPECT_NO_THROW(s[99]);
+
+	Stack<int, 24> s2;
+	for(size_t i = 0; i < 100; i++) {
+		s2.Push(i);
+	}
+	
+	EXPECT_EQ(s2[0], 0);
+	EXPECT_EQ(s2[21], 21);
+	EXPECT_EQ(s2[68], 68);
+	EXPECT_EQ(s2[99], 99);
+
+	EXPECT_THROW(s2[-1], std::out_of_range);
+	EXPECT_THROW(s2[-100], std::out_of_range);
+	EXPECT_THROW(s2[100], std::out_of_range);
+	EXPECT_THROW(s2[1000], std::out_of_range);
+
+	EXPECT_NO_THROW(s2[0]);
+	EXPECT_NO_THROW(s2[10]);
+	EXPECT_NO_THROW(s2[52]);
+	EXPECT_NO_THROW(s2[99]);
+}
+
+TEST(StackTest, Clear) {
+	Stack<int> s;
+	for(size_t i = 0; i < 100; i++) {
+		s.Push(i);
+	}
+	size_t maxSize = s.MaxLength();
+	s.Clear();
+	EXPECT_EQ(s.Length(), 0);
+	EXPECT_THROW(s.Peek(), std::out_of_range);
+	EXPECT_THROW(s.Pop(), std::out_of_range);
+	EXPECT_EQ(maxSize, s.MaxLength());
+	EXPECT_THROW(s[-1], std::out_of_range);
+	EXPECT_THROW(s[-100], std::out_of_range);
+	EXPECT_THROW(s[0], std::out_of_range);
+	EXPECT_THROW(s[1], std::out_of_range);
+	EXPECT_THROW(s[1000], std::out_of_range);
+
+	Stack<int, 10> s2;
+	for(size_t i = 0; i < 100; i++) {
+		s2.Push(i);
+	}
+	size_t maxSize2 = s2.MaxLength();
+	s2.Clear();
+	EXPECT_EQ(s2.Length(), 0);
+	EXPECT_THROW(s2.Peek(), std::out_of_range);
+	EXPECT_THROW(s2.Pop(), std::out_of_range);
+	EXPECT_EQ(maxSize2, s2.MaxLength());
+	EXPECT_THROW(s2[-1], std::out_of_range);
+	EXPECT_THROW(s2[-100], std::out_of_range);
+	EXPECT_THROW(s2[0], std::out_of_range);
+	EXPECT_THROW(s2[1], std::out_of_range);
+	EXPECT_THROW(s2[1000], std::out_of_range);
+}
+
+
+
+
 int main(int argc, char** argv) {
 	testing::InitGoogleTest();
 	return RUN_ALL_TESTS();
 }
-
