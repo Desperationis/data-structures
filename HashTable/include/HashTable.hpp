7a50650b4ded197faac9b803930514db2c652919
Desperationis <smarttdiego@gmail.com>
2022-06-01 10:10:23 -0700

    Add the basic framework for HashTable

---
+++ HashTable/include/HashTable.hpp
@@ -0,0 +1,18 @@
+#ifndef HASHTABLE_HPP
+#define HASHTABLE_HPP
+
+#include <memory>
+#include <iostream>
+#include <cstring>
+#include "LinkedList.hpp"
+
+class HashTable {
+public:
+	HashTable() { }
+
+private:
+	char* data;
+};
+
+
+#endif
---
+++ HashTable/src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "HashTable.hpp"
+#include <gtest/gtest.h>
+
+int main(int argc, char** argv) {
+	testing::InitGoogleTest();
+	return RUN_ALL_TESTS();
+}
