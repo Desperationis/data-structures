b8ccd42cd26e221c2e398cc819358608fbb21a15
Desperationis <smarttdiego@gmail.com>
2022-06-01 09:21:50 -0700

    Greatly improve LinkedList API
    In preparation for the HashTable, improve LinkedList that adds a
    universal Remove(index) and Insert(index, val) functions so that
    PushBack and PopBack are easily available as well.

--- LinkedList/include/LinkedList.hpp
+++ LinkedList/include/LinkedList.hpp
@@ -10,8 +10,8 @@ class LinkedList {
 public:
 	LinkedList() {
 		head = nullptr;
+		end = nullptr;
 		length = 0;
-
 	}
 
 	~LinkedList() {
@@ -23,25 +23,78 @@ public:
 	}
 
 	void PushFront(T value) {
-		Node<T>* newNode = new Node<T>(value, head);
-		head = newNode;
-		length++;
+		Insert(0, value);
 	};
+
+	void PushBack(T value) {
+		Insert(Length(), value);
+	}
 
 	T PopFront() {
-		if(head == nullptr)
-			return 0;
-
-		Node<T>* second = head->next;
-		delete head;
-		head = second;
-		length--;
-		
-		if(head == nullptr)
-			return 0;
-
-		return head->value;
+		return Remove(0);
 	};
+
+	void Insert(size_t index, T value) {
+		Node<T>* nodeToInsert = new Node<T>(value, nullptr);
+		if(length == 0) {
+			head = end = nodeToInsert;
+			length++;
+			return;
+		}
+
+		if(index == 0) {
+			nodeToInsert->next = head;
+			head = nodeToInsert;
+			length++;
+			return;
+		}
+
+		Node<T>* nodeAtIndex = FindNode(index - 1);
+
+		nodeToInsert->next = nodeAtIndex->next;
+		nodeAtIndex->next = nodeToInsert;
+
+		if(nodeAtIndex == end) 
+			end = nodeToInsert;
+
+		length++;
+	}
+
+	T Remove(size_t index) {
+		Node<T>* nodeToRemove = FindNode(index);
+
+		// If there is more than one node we need to repair the previous node.
+		// If one of them is `head` or `end` we need to repair those as well.
+		if(length > 1) {
+			if(nodeToRemove != head) {
+				Node<T>* previousNode = FindNode(index - 1);
+				previousNode->next = nodeToRemove->next;
+				if(nodeToRemove == end) {
+					end = previousNode;
+				}
+			}
+
+			if(nodeToRemove == head)
+				head = nodeToRemove->next;
+		}
+	
+		T value = nodeToRemove->value;
+		delete nodeToRemove;
+		nodeToRemove = nullptr;
+		length--;
+
+		if(length == 0) {
+			head = nullptr;
+			end = nullptr;
+		}
+
+		return value;
+	}
+
+
+	T PopBack() {
+		return Remove(Length() - 1);
+	}
 
 	size_t Length() {
 		return length;
@@ -57,6 +110,10 @@ public:
 	}
 
 private:
+	Node<T>* FindNode(size_t index) {
+		return GetNode(index, head, 0);
+	}
+	
 	Node<T>* GetNode(size_t targetIndex, Node<T>* currentNode, size_t currentIndex) {
 		if(targetIndex < 0 || targetIndex >= length)
 			throw std::out_of_range("Attempted to access element out of bounds!");
@@ -68,7 +125,7 @@ private:
 	}
 
 private:
-	Node<T>* head;
+	Node<T> *head, *end;
 	size_t length;
 };
 
--- LinkedList/src/main.cpp
+++ LinkedList/src/main.cpp
@@ -2,7 +2,37 @@
 #include "LinkedList.hpp"
 #include <gtest/gtest.h>
 
-TEST(LinkedListTest, Populate) {
+TEST(LinkedListTest, InsertRemove) {
+	LinkedList<int> list;
+	for(size_t i = 0; i < 100; i++ ) {
+		list.Insert(i, i * i);
+	}
+
+	// From high to low to avoid messing with previous indeces
+	EXPECT_EQ(list.Remove(99), 99 * 99);	
+	EXPECT_EQ(list.Remove(38), 38 * 38);	
+	EXPECT_EQ(list.Remove(0), 0);	
+
+	EXPECT_EQ(list.Remove(0), 1);	
+	EXPECT_EQ(list.Remove(0), 4);	
+
+	list.Insert(0, 14270);
+	EXPECT_EQ(list.Remove(0), 14270);	
+
+	list.Insert(39, 19230);
+	EXPECT_EQ(list.Remove(39), 19230);	
+
+	EXPECT_THROW(list.Remove(10000), std::out_of_range);
+	EXPECT_THROW(list.Remove(-1), std::out_of_range);
+	EXPECT_THROW(list.Remove(95), std::out_of_range);
+
+	EXPECT_THROW(list.Insert(96, 1), std::out_of_range);
+	EXPECT_THROW(list.Insert(10000, 1), std::out_of_range);
+	EXPECT_THROW(list.Insert(-1, 1), std::out_of_range);
+}
+
+
+TEST(LinkedListTest, PopulateFront) {
 	LinkedList<int> list;
 	for(size_t i = 0; i < 20; i++) {
 		list.PushFront(i);
@@ -13,7 +43,37 @@ TEST(LinkedListTest, Populate) {
 	}
 }
 
-TEST(LinkedListTest, LengthTest) {
+TEST(LinkedListTest, PopulateBack) {
+	LinkedList<int> list;
+	for(size_t i = 0; i < 20; i++) {
+		list.PushBack(i);
+	}
+
+	for(size_t i = 0; i < 20; i++) {
+		EXPECT_EQ(list.PopFront(), i);
+	}
+}
+
+TEST(LinkedListTest, PopBack) {
+	LinkedList<int> list2;
+	for(size_t i = 0; i < 20; i++) {
+		list2.PushFront(i);
+	}
+	for(size_t i = 0; i < 20; i++) {
+		EXPECT_EQ(list2.PopBack(), i);
+	}
+
+	LinkedList<int> list;
+	for(size_t i = 0; i < 20; i++) {
+		list.PushBack(i);
+	}
+
+	for(int i = 19; i >= 0; i--) { // Must be int for negatives
+		EXPECT_EQ(list.PopBack(), i);
+	}
+}
+
+TEST(LinkedListTest, LengthTestFront) {
 	LinkedList<int> list;
 	for(size_t i = 0; i < 148; i++) {
 		list.PushFront(i);
@@ -23,14 +83,54 @@ TEST(LinkedListTest, LengthTest) {
 	EXPECT_EQ(list.Length(), 0);
 
 	LinkedList<int> list2;
-	EXPECT_EQ(list.Length(), 0);
+	EXPECT_EQ(list2.Length(), 0);
 	for(size_t i = 0; i < 148; i++) {
-		list.PushFront(i);
+		list2.PushFront(i);
 	}
 	for(size_t i = 0; i < 148; i++) {
-		list.PopFront();
+		list2.PopFront();
 	}
+	EXPECT_EQ(list2.Length(), 0);
+
+	LinkedList<int> list3;
+	EXPECT_EQ(list3.Length(), 0);
+	for(size_t i = 0; i < 148; i++) {
+		list3.PushFront(i);
+	}
+	for(size_t i = 0; i < 148; i++) {
+		list3.PopBack();
+	}
+	EXPECT_EQ(list3.Length(), 0);
+}
+
+TEST(LinkedListTest, LengthTestBack) {
+	LinkedList<int> list;
+	for(size_t i = 0; i < 148; i++) {
+		list.PushBack(i);
+	}
+	EXPECT_EQ(list.Length(), 148);
+	list.Clear();
 	EXPECT_EQ(list.Length(), 0);
+
+	LinkedList<int> list2;
+	EXPECT_EQ(list2.Length(), 0);
+	for(size_t i = 0; i < 148; i++) {
+		list2.PushBack(i);
+	}
+	for(size_t i = 0; i < 148; i++) {
+		list2.PopFront();
+	}
+	EXPECT_EQ(list2.Length(), 0);
+
+	LinkedList<int> list3;
+	EXPECT_EQ(list3.Length(), 0);
+	for(size_t i = 0; i < 148; i++) {
+		list3.PushBack(i);
+	}
+	for(size_t i = 0; i < 148; i++) {
+		list3.PopBack();
+	}
+	EXPECT_EQ(list3.Length(), 0);
 }
 
 TEST(LinkedListTest, Bounds) {
@@ -46,6 +146,19 @@ TEST(LinkedListTest, Bounds) {
 	EXPECT_THROW(list[2000], std::out_of_range);
 	EXPECT_THROW(list[-1], std::out_of_range);
 	EXPECT_THROW(list[-1000], std::out_of_range);
+
+	LinkedList<int> list2;
+	for(size_t i = 0; i < 20; i++) {
+		list2.PushBack(i);
+	}
+
+	EXPECT_NO_THROW(list2[0]);
+	EXPECT_NO_THROW(list2[5]);
+	EXPECT_NO_THROW(list2[19]);
+	EXPECT_THROW(list2[20], std::out_of_range);
+	EXPECT_THROW(list2[2000], std::out_of_range);
+	EXPECT_THROW(list2[-1], std::out_of_range);
+	EXPECT_THROW(list2[-1000], std::out_of_range);
 }
 
 TEST(LinkedListTest, Access) {
@@ -58,6 +171,16 @@ TEST(LinkedListTest, Access) {
 	EXPECT_EQ(list[199 - 34], 34 * 2);
 	EXPECT_EQ(list[199 - 130], 130 * 2);
 	EXPECT_EQ(list[199 - 199], 199 * 2);
+
+	LinkedList<int> list2;
+	for(size_t i = 0; i < 200; i++) {
+		list2.PushBack(i * 2);
+	}
+
+	EXPECT_EQ(list2[0], 0);
+	EXPECT_EQ(list2[34], 34 * 2);
+	EXPECT_EQ(list2[130], 130 * 2);
+	EXPECT_EQ(list2[199], 199 * 2);
 }
 
 TEST(LinkedListTest, Clear) {
@@ -68,7 +191,9 @@ TEST(LinkedListTest, Clear) {
 	list.Clear();
 	EXPECT_THROW(list[0], std::out_of_range);
 	EXPECT_THROW(list[20], std::out_of_range);
-	EXPECT_NO_THROW(list.PopFront());
+	EXPECT_THROW(list.PopFront(), std::out_of_range);
+	EXPECT_THROW(list.PopBack(), std::out_of_range);
+	EXPECT_THROW(list.Remove(0), std::out_of_range);
 }
 
 TEST(LinkedListTest, Init) {
@@ -76,7 +201,10 @@ TEST(LinkedListTest, Init) {
 	EXPECT_THROW(list[0], std::out_of_range);
 	EXPECT_THROW(list[20], std::out_of_range);
 	EXPECT_EQ(list.Length(), 0);
-	EXPECT_NO_THROW(list.PopFront());
+	EXPECT_THROW(list.PopFront(), std::out_of_range);
+	EXPECT_THROW(list.PopBack(), std::out_of_range);
+	EXPECT_THROW(list.Remove(0), std::out_of_range);
+	EXPECT_NO_THROW(list.Insert(0, 1));
 }
 
 int main(int argc, char** argv) {
